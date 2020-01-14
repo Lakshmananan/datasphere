fa1de07907c15061538d56b0d4838a2761285139
lakshmananan <lakshman@datasphere.ca>
2020-01-14 13:10:37 -0500

    Added jsmol - needs to be fixed.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
+import Test from './components/Test';
+
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+
 function App() {
-  return <div></div>;
+  useEffect(() => {
+    M.AutoInit();
+  });
+
+  return (
+    <div>
+      <Test />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Test = () => {
+  return (
+    <div>
+      <div id='glmol01' style={{ width: '350px', height: '350px' }}></div>
+      <textarea id='glmol01_src' style={{ display: 'none' }}></textarea>
+    </div>
+  );
+};
+
+export default Test;
